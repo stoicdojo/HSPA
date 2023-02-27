abe2e15aa87db644c22313f9e1fecc9432e62d59
radwinski <stoicdojo@gmail.com>
2023-02-26 19:42:40 -0700

    2nd Commit

--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
+  properties: Array<IProperty> = [];
 
+  constructor(private housingService:HousingService) {
+
+  }
+
+  ngOnInit() : void {
+    this.housingService.getAllProperties().subscribe(
+      data=> this.properties = data
+    ), (error: any) => {
+      console.log(error);
+    };
+  }
 }
